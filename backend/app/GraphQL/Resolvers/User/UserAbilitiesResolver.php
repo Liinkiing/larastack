aa4658f1e267569b618a4b0e3e5b57b277e81f10
Omar Jbara <omar.jbara2@gmail.com>
2025-01-24 17:33:03 +0100

    fix: lint

--- backend/app/GraphQL/Resolvers/User/UserAbilitiesResolver.php
+++ backend/app/GraphQL/Resolvers/User/UserAbilitiesResolver.php
@@ -7,7 +7,6 @@ use App\Models\User;
 final class UserAbilitiesResolver
 {
     /**
-     * @param User $root
      * @return array{viewApp: bool}
      */
     public function __invoke(User $root): array
