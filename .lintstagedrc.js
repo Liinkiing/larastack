640f7eb10c38bc72b208a6bc809e5e80e4d42f74
Omar Jbara <omar.jbara2@gmail.com>
2025-01-24 17:29:42 +0100

    chore: added pre commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "*.{js,jsx,ts,tsx}": ["eslint --cache --fix"],
-    "*": "prettier --ignore-unknown --write",
-};
+  '*.{js,jsx,ts,tsx}': ['eslint --cache --fix'],
+  '*.{js,jsx,ts,tsx,json,md,mdx,svg,html,css}': 'prettier --ignore-unknown --write',
+}
