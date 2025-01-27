0905d739c0ac5bf4fadc723e835f0979bcd0e9e2
Omar Jbara <omar.jbara2@gmail.com>
2025-01-27 07:31:10 +0100

    fix: set logged in middleware

--- backend/app/Http/Middleware/SetLoggedInCookieMiddleware.php
+++ backend/app/Http/Middleware/SetLoggedInCookieMiddleware.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 
 class SetLoggedInCookieMiddleware
 {
-    public function handle(Request $request, Closure $next): Closure
+    /** @phpstan-ignore missingType.return */
+    public function handle(Request $request, Closure $next)
     {
         Cookie::queue(
             Cookie::forever(
                 config('session.logged_in_cookie_key'),
-                /** @phpstan-ignore property.notFound */
                 Auth::check() ? base64_encode('User:'.Auth::user()->id) : 'false',
                 httpOnly: false
             )
