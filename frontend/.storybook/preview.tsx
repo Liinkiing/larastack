4b4b0e3294768e17de49aff20b53fd2501aed261
Omar Jbara <omar.jbara2@gmail.com>
2026-02-01 07:14:19 +0100

    chore: upgrade storybook

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -8,10 +8,7 @@ import { fonts } from '../app/fonts'
 const preview: Preview = {
   decorators: [
     Story => (
-      <div
-        className={`${fonts.body.variable} ${fonts.display.variable} ${fonts.mono.variable}`}
-        style={{ display: 'contents' }}
-      >
+      <div className={`${fonts.geistSans.variable} ${fonts.geistMono.variable}`} style={{ display: 'contents' }}>
         <Story />
       </div>
     ),
