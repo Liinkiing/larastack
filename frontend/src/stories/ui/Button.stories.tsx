8a83bcc73d3d32db970f3104f162a5dc1d957ef3
Omar Jbara <omar.jbara2@gmail.com>
2026-02-01 07:15:05 +0100

    chore: upgrade storybook

--- frontend/src/stories/ui/Button.stories.tsx
+++ frontend/src/stories/ui/Button.stories.tsx
@@ -1,15 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
 
-import { button } from '~/styled-system/recipes'
 import { Button } from '~/ui/button'
-import { generatePandaVariantsArgTypes } from '~/utils/storybook'
 
 const meta = {
-  argTypes: generatePandaVariantsArgTypes(button),
   args: {
     children: 'Label',
-    size: 'md',
-    variant: 'solid',
   },
   component: Button,
   title: 'UI/Button',
@@ -18,26 +13,4 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Solid = {
-  args: {
-    variant: 'solid',
-  },
-} satisfies Story
-
-export const Soft = {
-  args: {
-    variant: 'soft',
-  },
-} satisfies Story
-
-export const Ghost = {
-  args: {
-    variant: 'ghost',
-  },
-} satisfies Story
-
-export const Outline = {
-  args: {
-    variant: 'Outline',
-  },
-}
+export const Primary = {} satisfies Story
